08da6572a2cd7265b12a4ea8403b15bf026e4516
Joaquim Alves <jafn92@gmail.com>
2019-05-20 23:21:53 -0300

    second commit

--- index.js
+++ index.js
@@ -4,13 +4,39 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.render('home');
+const sqlite = require('sqlite');
+const dbConnection = sqlite.open('banco.sqlite', { Promise });
+
+app.get('/', async(req, res) => {
+    const db = await dbConnection;
+    const categoriasDb = await db.all('SELECT * FROM categorias;'); 
+    const vagas = await db.all('SELECT * FROM vagas;');
+    const categorias = categoriasDb.map(cat => {
+        return {
+            ...cat,
+            vagas: vagas.filter(vaga => vagas.categoria === cat.id)
+        }
+    });
+    res.render('home', {
+        categorias
+    });
 });
 
 app.get('/vaga', (req, res) => {
     res.render('vaga');
 });
+
+const init = async() => {
+    const db = await dbConnection;
+    await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT)');
+    await db.run('create table if not exists vagas (id INTEGER PRIMARY KEY, categoria INTEGER, titulo TEXT, descricao TEXT)');
+    //const categoria = 'Engeneering Team';
+    //await db.run(`INSERT INTO categorias (categoria) VALUES ('${categoria}')`);
+    //const vaga = 'BackEnd Developer (Remote)';
+    //const descricao = 'Vaga para BackEnd Developer que fez o FullStack Lab';
+    //await db.run(`INSERT INTO vagas (categoria, titulo, descricao) VALUES (2, '${vaga}', '${descricao}')`);
+}
+init();
 
 app.listen(3000, (err) => {
     if(err){
